c8e0e4f5566b093e914ba172ad7fa46872b15fe8
z85004510 <85004510@qq.com>
2019-02-21 15:13:01 +0800

    create

---
+++ src/main/java/com/zhang/taxcalculator/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.zhang.taxcalculator;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TaxCalculatorApplication.class);
+    }
+
+}
